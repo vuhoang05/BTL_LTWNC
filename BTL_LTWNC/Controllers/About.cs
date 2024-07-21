e6ef5ecef8dd1c43e301e6924ed2b2cd1c0f7866
Vu huy hoang <hoangvuzz0512@gmail.com>
2024-07-22 01:15:55 +0700

    fix frontend

---
+++ BTL_LTWNC/Controllers/About.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_LTWNC.Controllers
+{
+    public class About : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BTL_LTWNC/Controllers/HomeController.cs
+++ BTL_LTWNC/Controllers/HomeController.cs
@@ -30,6 +30,7 @@ namespace BTL_LTWNC.Controllers
         {
             return View();
         }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
